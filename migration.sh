1e6b59745bc87d0b7a9d230db926007364625f3b
vitor <vitorsilvestri1@gmail.com>
2020-08-17 16:56:39 -0300

    Migrations can be created

--- migration.sh
+++ migration.sh
@@ -4,7 +4,26 @@
 . './utils.sh'
 . './template.sh'
 
+# Find a migration by name.
+# $1 => Name of the migration
+findMigration() {
+  find "$MIGRATIONS_DIR" -type f -name "$1"
+}
+
+create() {
+  local name="${1// /_}"
+  [[ $name =~ ^[a-zA-Z0-9_.\-]+$ ]] || end "Invalid characters in '$name'."
+  findMigration "*-$name.sql" | grep -q "$name" && end "Migration '$name' already exists."
+
+  local filename="$MIGRATIONS_DIR/$(date +"%s")-$name.sql"
+  migrationTemplate "$filename" "$name"
+
+  [[ "$?" -eq 0 ]]\
+    && (echo -e "Migration file '$filename' created." && return 0)\
+    || (end 'Error while creating migration...' && return 1)
+}
+
 checkDirectory "$MIGRATIONS_DIR"
 
-test_file="MyMigrationFile"
-migrationTemplate "$MIGRATIONS_DIR/$(date +"%s")-$test_file" "$test_file"
+test="MyTestMigration"
+create "$test"
--- utils.sh
+++ utils.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# End script to stderr
+# $1 => ERROR MESSAGE
+end() {
+  cat >&2 << EOF
+Usage: $SCRIPT <action>
+
+EOF
+  [[ $# -gt 0 ]] && echo "$SCRIPT: $1" >&2
+  exit 1
+}
+
 # Creates directory if it doesn't exist.
 # $1 => DIRECTORY NAME
 checkDirectory() {
